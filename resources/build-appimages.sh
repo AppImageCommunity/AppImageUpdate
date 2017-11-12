a6c5243e4a5004f5eacb779d95198213beedd5c9
TheAssassin <theassassin@users.noreply.github.com>
2017-11-12 23:16:14 +0100

    Bundle window icon (CC #38)

--- resources/build-appimages.sh
+++ resources/build-appimages.sh
@@ -40,7 +40,7 @@ make -j$(nproc) install DESTDIR=AppDir
 # install resources into AppDir
 mkdir -p AppDir/usr/share/{applications,icons/hicolor/scalable}
 cp -v "$REPO_ROOT"/resources/*.desktop AppDir/usr/share/applications
-cp -v "$REPO_ROOT"/resources/*.svg AppDir/usr/share/icons/hicolor/scalable
+cp -v "$REPO_ROOT"/resources/*.{svg,xpm} AppDir/usr/share/icons/hicolor/scalable
 
 # get linuxdeployqt
 wget https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage
