c4f76c7f539a82040e0544ae73c222d9a6ea00ea
antony-jr <antonyjr@protonmail.com>
2018-01-25 14:16:19 +0530

    Removing Objdump and using light_elf.h for information extraction from type 2 AppImages.

--- src/util.h
+++ src/util.h
@@ -7,13 +7,25 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include "light_elf.h"
 
 namespace appimage {
     namespace update {
-        static void removeNewlineCharacters(std::string& str) {
-            str.erase(std::remove(str.begin(), str.end(), '\n'), str.end());
-        }
+        static std::vector<std::string> split(const std::string& s, char delim = ' ') {
+            std::vector<std::string> result;
 
+            std::stringstream ss(s);
+            std::string item;
+
+            while (std::getline(ss, item, delim)) {
+                result.push_back(item);
+            }
+
+            return result;
+        }
         static inline bool ltrim(std::string& s, char to_trim = ' ') {
             // TODO: find more efficient way to check whether elements have been removed
             size_t initialLength = s.length();
@@ -22,7 +34,7 @@ namespace appimage {
             }));
             return s.length() < initialLength;
         }
-
+        
         static inline bool rtrim(std::string& s, char to_trim = ' ') {
             // TODO: find more efficient way to check whether elements have been removed
             auto initialLength = s.length();
@@ -37,51 +49,7 @@ namespace appimage {
             auto ltrim_result = ltrim(s, to_trim);
             return rtrim(s, to_trim) && ltrim_result;
         }
-
-        static bool callProgramAndGrepForLine(const std::string& command, const std::string& pattern,
-                                              std::string& output) {
-            FILE *stream = popen(command.c_str(), "r");
-
-            if (stream == nullptr)
-                return false;
-
-            char *line;
-            size_t lineSize = 0;
-            while(getline(&line, &lineSize, stream)) {
-                // check whether line matches pattern
-                std::string lineString = line;
-                if (lineString.find(pattern) != std::string::npos) {
-                    if (pclose(stream) != 0) {
-                        free(line);
-                        return false;
-                    }
-                    output = line;
-                    removeNewlineCharacters(output);
-                    return true;
-                }
-            }
-
-            if (pclose(stream) != 0) {
-                free(line);
-                return false;
-            }
-
-            return false;
-        }
-
-        static std::vector<std::string> split(const std::string& s, char delim = ' ') {
-            std::vector<std::string> result;
-
-            std::stringstream ss(s);
-            std::string item;
-
-            while (std::getline(ss, item, delim)) {
-                result.push_back(item);
-            }
-
-            return result;
-        }
-
+        
         static inline std::string toLower(std::string s) {
             std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c) { return std::tolower(c); });
             return s;
