6166995726a1b0cb51482bcc1b3819ae549a67ee
TheAssassin <theassassin@users.noreply.github.com>
2018-03-08 15:30:41 +0100

    Fix build issues, mk. 1

--- resources/build-appimages.sh
+++ resources/build-appimages.sh
@@ -131,7 +131,8 @@ rm *.desktop && cp usr/share/applications/AppImageUpdate-Qt.desktop .
 popd
 
 # bundle application
-./linuxdeployqt-continuous-x86_64.AppImage \
+env LD_LIBRARY_PATH="AppDir/usr/lib/" \
+    ./linuxdeployqt-continuous-x86_64.AppImage \
     AppDir/usr/share/applications/AppImageUpdate-Qt.desktop \
     -verbose=1 -bundle-non-qt-libs
 
