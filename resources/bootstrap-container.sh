a62e9b9cf021934600d2163b422f05e13bf850cb
TheAssassin <theassassin@users.noreply.github.com>
2019-06-15 22:32:30 +0200

    Use xenial repository

--- resources/bootstrap-container.sh
+++ resources/bootstrap-container.sh
@@ -5,8 +5,8 @@ if ! which wget 2>&1 1>/dev/null; then
     apt-get install -y wget
 fi
 
-echo 'deb http://download.opensuse.org/repositories/home:/TheAssassin:/AppImageLibraries/xUbuntu_14.04/ /' > /etc/apt/sources.list.d/curl-httponly.list
-wget -nv https://download.opensuse.org/repositories/home:TheAssassin:AppImageLibraries/xUbuntu_14.04/Release.key -O- | sudo apt-key add -
+echo 'deb http://download.opensuse.org/repositories/home:/TheAssassin:/AppImageLibraries/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/curl-httponly.list
+wget -nv https://download.opensuse.org/repositories/home:TheAssassin:AppImageLibraries/xUbuntu_16.04/Release.key -O- | sudo apt-key add -
 apt-get update
 apt-get install -y git g++ libxpm-dev libcurl3 libcurl4-openssl-dev automake libtool desktop-file-utils build-essential clang g++ libssl-dev pkg-config
 
