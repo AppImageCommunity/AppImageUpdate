1f52c72531283d095a49c37b0577ae5a34991d4a
TheAssassin <theassassin@users.noreply.github.com>
2020-12-13 08:15:02 +0100

    Remove Docker images' dependency on uid

--- ci/build-in-docker.sh
+++ ci/build-in-docker.sh
@@ -12,8 +12,7 @@ cwd="$PWD"
 repo_root="$(readlink -f "$(dirname "$0")"/..)"
 
 # needed to keep user ID in and outside Docker in sync to be able to write to workspace directory
-uid="$(id -u)"
-image=appimageupdate-build:"$DIST"-"$ARCH"-uid"$uid"
+image=appimageupdate-build:"$DIST"-"$ARCH"
 dockerfile=Dockerfile."$DIST"-"$ARCH"
 
 if [ ! -f "$repo_root"/ci/"$dockerfile" ]; then
@@ -22,10 +21,14 @@ if [ ! -f "$repo_root"/ci/"$dockerfile" ]; then
 fi
 
 # building local image to "cache" installed dependencies for subsequent builds
-docker build -t "$image" -f "$repo_root"/ci/"$dockerfile" --build-arg UID="$uid" "$repo_root"/ci
+docker build -t "$image" -f "$repo_root"/ci/"$dockerfile" "$repo_root"/ci
 
+# run the build with the current user to
+#   a) make sure root is not required for builds
+#   b) allow the build scripts to "mv" the binaries into the /out directory
+uid="$(id -u)"
 # mount workspace read-only, trying to make sure the build doesn't ever touch the source code files
 # of course, this only works reliably if you don't run this script from that directory
 # but it's still not the worst idea to do so
-docker run --rm -i -e ARCH -e GITHUB_RUN_NUMBER -e CI=1 -v "$repo_root":/ws:ro -v "$cwd":/out "$image" \
+docker run --user "$uid" --rm -i -e ARCH -e GITHUB_RUN_NUMBER -e CI=1 -v "$repo_root":/ws:ro -v "$cwd":/out "$image" \
     bash -xec 'cd /out && bash -xe /ws/ci/build-appimages.sh'
