7c79d265744dd41cbb234702f8c282d0a45401ec
TheAssassin <theassassin@assassinate-you.net>
2022-01-17 01:28:06 +0100

    Rework linking and add export config
    Also uses GNUInstallDirs to improve the target locations of the installed headers, libraries and configs.

---
+++ cmake/export.cmake
@@ -0,0 +1,40 @@
+# make sure CMake won't export this package by default (valid from 3.15 onwards)
+cmake_minimum_required(VERSION 3.15)
+set(CMP0090 NEW)
+
+# allow import from current build tree
+export(
+    TARGETS libappimageupdate
+    NAMESPACE AppImageUpdate::
+    FILE AppImageUpdateTargets.cmake
+)
+
+# allow import from install tree
+# note that the targets must be install(... EXPORT zsync) in order for this to work (consider libappimageupdate a namespace)
+install(
+    EXPORT AppImageUpdateTargets
+    DESTINATION lib/cmake/AppImageUpdate
+)
+
+include(CMakePackageConfigHelpers)
+# generate the config file that is includes the exports
+configure_package_config_file(
+    "${CMAKE_CURRENT_LIST_DIR}/AppImageUpdateConfig.cmake.in"
+    "${PROJECT_BINARY_DIR}/cmake/AppImageUpdateConfig.cmake"
+    INSTALL_DESTINATION "lib/cmake/AppImageUpdate"
+    NO_SET_AND_CHECK_MACRO
+    NO_CHECK_REQUIRED_COMPONENTS_MACRO
+)
+
+write_basic_package_version_file(
+    "${PROJECT_BINARY_DIR}/cmake/AppImageUpdateConfigVersion.cmake"
+    VERSION "${VERSION}"
+    COMPATIBILITY AnyNewerVersion
+)
+
+install(
+    FILES
+    "${PROJECT_BINARY_DIR}/cmake/AppImageUpdateConfig.cmake"
+    "${PROJECT_BINARY_DIR}/cmake/AppImageUpdateConfigVersion.cmake"
+    DESTINATION lib/cmake/AppImageUpdate
+)
