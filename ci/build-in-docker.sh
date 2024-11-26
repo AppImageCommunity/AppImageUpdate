ed57cc333cfa16c23dde41fcc91e76e2055dfce1
TheAssassin <theassassin@assassinate-you.net>
2024-11-26 01:45:37 +0100

    Fix Docker builds and add ARM support

--- ci/build-in-docker.sh
+++ ci/build-in-docker.sh
@@ -1,35 +1,67 @@
 #! /bin/bash
 
-if [[ "$DIST" == "" ]] || [[ "$ARCH" == "" ]]; then
-    echo "Usage: env ARCH=... DIST=... bash $0"
+if [[ "$ARCH" == "" ]]; then
+    echo "Usage: env ARCH=... bash $0"
     exit 1
 fi
 
-set -e
-set -x
+set -euxo pipefail
+
+case "$ARCH" in
+    x86_64)
+        docker_platform=linux/amd64
+        ;;
+    i686)
+        CMAKE_ARCH=i386
+        docker_platform=linux/386
+        ;;
+    armhf)
+        docker_platform=linux/arm/v7
+        ;;
+    aarch64)
+        docker_platform=linux/arm64/v8
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 2
+esac
+
+CMAKE_ARCH="${CMAKE_ARCH:-"$ARCH"}"
 
 cwd="$PWD"
 repo_root="$(readlink -f "$(dirname "$0")"/..)"
 
 # needed to keep user ID in and outside Docker in sync to be able to write to workspace directory
-image=appimageupdate-build:"$DIST"-"$ARCH"
-dockerfile=Dockerfile."$ARCH"
-
-if [ ! -f "$repo_root"/ci/"$dockerfile" ]; then
-    echo "Error: $dockerfile could not be found"
-    exit 1
-fi
+image=appimageupdate-build
 
 # building local image to "cache" installed dependencies for subsequent builds
-docker build -t "$image" -f "$repo_root"/ci/"$dockerfile" --build-arg DIST="$DIST" "$repo_root"/ci
+docker build \
+    --platform "$docker_platform" \
+    -t "$image" \
+    --build-arg ARCH="$ARCH" \
+    --build-arg CMAKE_ARCH="$CMAKE_ARCH" \
+    "$repo_root"/ci
 
 # run the build with the current user to
 #   a) make sure root is not required for builds
 #   b) allow the build scripts to "mv" the binaries into the /out directory
 uid="$(id -u)"
+
+tty_args=()
+if [ -t 0 ]; then tty_args+=("-t"); fi
+
 # mount workspace read-only, trying to make sure the build doesn't ever touch the source code files
 # of course, this only works reliably if you don't run this script from that directory
 # but it's still not the worst idea to do so
-docker run --platform "$ARCH" \
-    --user "$uid" --rm -i -e ARCH -e GITHUB_RUN_NUMBER -e CI=1 -v "$repo_root":/ws:ro -v "$cwd":/out "$image" \
-    bash -xec 'cd /out && bash -xe /ws/ci/build-appimages.sh'
+docker run \
+    --rm \
+    -i \
+    "${tty_args[@]}" \
+    -e CI=1 \
+    -e GITHUB_RUN_NUMBER \
+    -v "$repo_root":/ws:ro \
+    -v "$cwd":/out \
+    -w /out \
+    --user "$uid" \
+    "$image" \
+    bash /ws/ci/build-appimages.sh
